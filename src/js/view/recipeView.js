cbb5e38a009e9562136235c670d19ec58f851951
Yash <yashharale2003@gmail.com>
2022-08-21 13:22:48 +0530

    Debugging

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -121,15 +121,15 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
       <div class="recipe__description">
-        <span class="recipe__unit">${ing.unit}</span>
-        ${ing.description}
+      <span class="recipe__unit">${ing.unit}</span>
+      ${ing.description}
       </div>
-    </li>
+      </li>
       `;
     }
   }
+  // <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
 
 
 export default new RecipeView();
