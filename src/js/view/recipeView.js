1f53c534d6c1bb95524a08ed85711702a07ee1a6
Yash <yashharale2003@gmail.com>
2022-08-21 13:23:46 +0530

    Debugging

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -3,7 +3,7 @@ import View from './View.js';
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 
-import {Fraction} from 'fractional';
+// import {Fraction} from 'fractional';
 // console.log(Fraction)
 
 class RecipeView extends View {
