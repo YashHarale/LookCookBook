c9ebfb2c09ea379a8bd5ea8aa839948f97ea2158
Yash <yashharale2003@gmail.com>
2022-08-21 17:18:44 +0530

    fractional

--- src/js/view/recipeView.js
+++ src/js/view/recipeView.js
@@ -3,7 +3,9 @@ import View from './View.js';
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 
-import {Fraction} from 'fractional';
+// import {Fraction} from 'fractional';
+// ? new Fraction(ing.quantity).toString() : ''
+
 // console.log(Fraction)
 
 class RecipeView extends View {
@@ -121,7 +123,7 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+      <div class="recipe__quantity">${ing.quantity}</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
         ${ing.description}
